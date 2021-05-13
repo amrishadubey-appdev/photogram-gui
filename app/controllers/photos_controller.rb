d93815818ce7b2df11582758b81b9519354312cf
amrishadubey <amrisha@chicagobooth.edu>
2021-05-13 13:06:25 +0000

    final submission photogram

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,61 @@
+class PhotosController < ApplicationController
+  def index
+   matching_photos = Photo.all
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
+    
+    render({ :template => "photos_templates/index.html.erb" })
+  end
+
+  def show
+    url_id = params.fetch("path_id")
+    @photo = Photo.where({ :id => url_id }).at(0)
+
+    render({:template => "photos_templates/show.html.erb"})
+  end
+
+  def delete
+   the_id = params.fetch("path_id")
+
+   matching_photos = Photo.where({ :id => the_id })
+   photo = matching_photos.at(0)
+   photo.destroy
+
+    redirect_to("/photos")
+  end
+
+  def create
+
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+    input_owner_id = params.fetch("query_owner_id")
+
+    new_photo = Photo.new
+    new_photo.image = input_image
+    new_photo.caption = input_caption
+    new_photo.owner_id = input_owner_id
+    
+    new_photo.save
+
+    next_url = "/photos/" + new_photo.id.to_s
+    redirect_to(next_url)
+
+  end
+
+  def update
+    
+    the_id = params.fetch("path_id")
+    input_image = params.fetch("input_image")
+    input_caption = params.fetch("input_caption")
+
+    matching_photos = Photo.where({ :id => the_id }) 
+    the_photo = matching_photos.first
+    the_photo.image = input_image
+    the_photo.caption = input_caption
+
+    the_photo.save
+
+    next_url = "/photos/"+ the_photo.id.to_s    
+    redirect_to(next_url)
+
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+  def index
+     matching_users = User.all
+      @list_of_users = matching_users.order({ :username => :asc })
+    render({ :template => "users_templates/index.html.erb" })
+  end
+
+  def show
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username })
+
+    @the_user = matching_usernames.at(0)
+
+      render({ :template => "users_templates/show.html.erb" })
+  end
+
+  def create
+
+    input_username = params.fetch("input_username")
+
+    new_user = User.new 
+    new_user.username = input_username
+    new_user.save
+
+    redirect_to("/users/"+ new_user.username.to_s)
+  end
+
+
+  def edit
+    input_username = params.fetch("input_username")
+
+    user_id = params.fetch("user_id")
+    
+    the_user = User.where({ :id => user_id }).first
+
+    the_user.username = input_username
+
+    the_user.save
+
+    redirect_to("/users/"+ the_user.username.to_s)
+  end
+
+end
