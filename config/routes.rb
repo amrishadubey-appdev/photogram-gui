6813037360dafb1fd699ba51aeed6f1d7e812abc
amrishadubey <amrisha@chicagobooth.edu>
2021-05-13 12:56:02 +0000

    new workspace

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "users", :action => "index" })
+
+  get("/users", { :controller => "users", :action => "index" })
+
+  get("/users/:path_username", { :controller => "users", :action => "show" })
+
+  get("/photos", { :controller => "photos", :action => "index" })
+
+  get("/photos/:path_id", { :controller => "photos", :action => "show"})
+
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "delete" })
+
+  get("/insert_photo", { :controller => "photos", :action => "create" })
+
+  get("/update_photo/:path_id", { :controller => "photos", :action => "update" })
+
+  get("/insert_comment_record", { :controller => "comments", :action => "create" })
+
+  get("/insert_user_record", { :controller => "users", :action => "create" })
+
+  get("/update_user/:user_id", { :controller => "users", :action => "edit" })
 
 end
